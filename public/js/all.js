039ddd40432bbbb88a73284270bfd978815f2e1e
James Gray <jgrayillustrate@gmail.com>
2015-07-12 06:24:40 -0400

    proc. JS

--- public/js/all.js
+++ public/js/all.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
         $('.timer-loader').show();
         window.location.href="/login";
     });
+    var options = $('#options').val();
+    $('#close').click(function() {
+        document.location="pebblejs://close#"+options;
+    });
 });
 
 //# sourceMappingURL=all.js.map
